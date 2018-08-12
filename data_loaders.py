c7bc2ea980c498c4d0cd03262da21e2e68ea80ff
kevinlyu <kevinlumail@gmail.com>
2018-08-12 21:03:47 +0800

    USPS dataloader part done

--- data_loaders.py
+++ data_loaders.py
@@ -29,7 +29,7 @@ class MNISTM(Dataset):
         else:
             img, target = self.test_data[index], self.test_label[index]
 
-        img = Image.fromarray(img.squeeze().numpy(), mode='RGB')
+        img = Image.fromarray(img.squeeze().numpy(), mode="RGB")
 
         if self.transform is not None:
             img = self.transform(img)
@@ -45,3 +45,46 @@ class MNISTM(Dataset):
             return len(self.train_data)
         else:
             return len(self.test_data)
+
+class USPS(Dataset):
+
+    def __init__(self, root="/home/neo/dataset/usps/", train=True, transform =None, target_transform=None):
+        super(USPS, self).__init__()
+        self.root = root
+        self.transform = transform
+        self.target_transform = target_transform
+        self.train = train
+
+        if self.train:
+            self.data , self.label = torch.load(os.path.join(self.root, "usps_pytorch_train"))
+        else:
+            self.data, self.label = torch.load(os.path.join(self.root, "usps_pytorch_test"))
+
+    def __getitem__(self, index):
+        data, label = self.data[index], self.label[index]
+        data = Image.fromarray(data,mode="RGB")
+
+        if self.transform is not None:
+            data = self.transform(data)
+
+        if self.target_transform is not None:
+            label = self.target_transform(label)
+        
+        return data, label
+
+    def __len__(self):
+        return len(self.label)
+
+
+'''
+import torchvision.transforms as transforms
+toy = torch.utils.data.DataLoader(MNISTM(transform=transforms.Compose([
+                       transforms.Resize((28,28)),
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                   ])),batch_size=1, shuffle=True)
+
+for index, (data, label) in enumerate (toy):
+    print(data)
+    print(label)
+'''
--- util.py
+++ util.py
@@ -5,32 +5,72 @@ from torch.utils.data import Dataset
 from torchvision import datasets, transforms
 import pickle
 import gzip
-import os 
+import os
+import h5py
 
-# reference: https://github.com/eriklindernoren/PyTorch-GAN/blob/master/implementations/pixelda/mnistm.py
-# mnistm pickle file: https://github.com/VanushVaswani/keras_mnistm/releases/download/1.0/keras_mnistm.pkl.gz 
+'''
+reference: https://github.com/eriklindernoren/PyTorch-GAN/blob/master/implementations/pixelda/mnistm.py
+mnistm pickle file: https://github.com/VanushVaswani/keras_mnistm/releases/download/1.0/keras_mnistm.pkl.gz
+'''
 
-def process_mnistm(mnistm_path = "/home/neo/dataset/mnistm/",mnist_path = "/home/neo/dataset/mnist/"):
+'''
+image after preprocess is in CHW(channel, height, width) format for pytorch use. If you want to plot 
+these images, convert them into HWC first
+'''
+
+def process_mnistm(mnistm_path="/home/neo/dataset/mnistm/", mnist_path="/home/neo/dataset/mnist/"):
     with open(os.path.join(mnistm_path, "mnistm.pkl"), "rb") as f:
         mnistm_data = pickle.load(f, encoding="bytes")
-    
+
     # load content of pickle file into pytorch tensor (data only)
     mnistm_train_data = torch.ByteTensor(mnistm_data[b'train'])
     mnistm_test_data = torch.ByteTensor(mnistm_data[b'test'])
-    
+
     # load label of mnist dataset
-    mnist_train_labels = datasets.MNIST(root=mnist_path, train=True,download=True).train_labels
-    mnist_test_labels = datasets.MNIST(root=mnist_path,train=False,download=True).test_labels
-    
+    mnist_train_labels = datasets.MNIST(
+        root=mnist_path, train=True, download=True).train_labels
+    mnist_test_labels = datasets.MNIST(
+        root=mnist_path, train=False, download=True).test_labels
+
     # combine (data, label)
     training_set = (mnistm_train_data, mnist_train_labels)
-    test_set = (mnistm_test_data, mnist_test_labels)
-    
+    testing_set = (mnistm_test_data, mnist_test_labels)
+
     # save mnist data with (data, label)
     with open(os.path.join(mnistm_path, "mnistm_pytorch_train"), "wb") as f:
         torch.save(training_set, f)
-        
+
     with open(os.path.join(mnistm_path, "mnistm_pytorch_test"), "wb") as f:
-        torch.save(test_set, f)
-    
+        torch.save(testing_set, f)
+
     print("Done!")
+
+
+import matplotlib.pyplot as plt
+
+
+def process_usps(usps_path="/home/neo/dataset/usps/"):
+
+    with h5py.File(os.path.join(usps_path, "usps.h5"), "r") as f:
+        train = f.get("train")
+        usps_train_data = train.get("data")[:]
+        usps_train_label = train.get("target")[:]
+        test = f.get("test")
+        usps_test_data = test.get("data")[:]
+        usps_test_label = test.get("target")[:]
+
+    usps_train_data = np.reshape(usps_train_data, (-1, 16, 16))
+    usps_test_data = np.reshape(usps_test_data, (-1, 16, 16))
+
+    #usps_train_data*=255.0
+    #usps_test_data*=255.0
+
+    training_set = (usps_train_data, usps_test_data)
+    testing_set = (usps_test_data, usps_test_label)
+
+    # save usps data with (data, label)
+    with open(os.path.join(usps_path, "usps_pytorch_train"), "wb") as f:
+        torch.save(training_set, f)
+
+    with open(os.path.join(usps_path, "usps_pytorch_test"), "wb") as f:
+        torch.save(testing_set, f)
